85f2652b7c5dbeb48a39710b05a0f04d6e83be9a
LiuXianfa <745024471@qq.com>
2021-02-23 19:53:17 +0800

    重新编译一次spring源码之后,这两个不正常的情况都解决了,主要原因就是以前编译的spring源码有问题.重新编译一次就行了.

--- spring-webmvc-test/src/main/java/com/mytest/controller/HelloController.java
+++ spring-webmvc-test/src/main/java/com/mytest/controller/HelloController.java
@@ -1,10 +1,8 @@
 package com.mytest.controller;
 
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,9 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  * @email xianfaliu@newbanker.cn
  * @date 2/16 23:34
  */
-@Controller
-@Order(value = 1)
-//@RestController // 这里有个坑:springmvc框架的DispatcherServlet默认使用的是 RequestMappingHandlerMapping ,这个HandlerMapping只会处理带有@Controller注解和@RequestMapping注解的bean.详见:其isHandler()方法
+@RestController
 public class HelloController {
 
     public HelloController() {
@@ -29,7 +25,6 @@ public class HelloController {
      * @return
      */
     @RequestMapping("/hello")
-    @ResponseBody
     public String hello(HttpServletRequest request, HttpServletResponse response, @RequestHeader("Host") String Host) {
         System.out.println(Host);
         return "hello,success!!!";
